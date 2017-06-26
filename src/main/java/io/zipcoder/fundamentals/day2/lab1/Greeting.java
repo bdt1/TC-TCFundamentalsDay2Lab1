eaedeb23204aaf26c782407331588ce5a4b33fed
Thomas <O581051@NAEAST.AD.JPMORGANCHASE.com>
2017-06-26 01:05:16 -0400

    Completed Day2Lab1

--- src/main/java/io/zipcoder/fundamentals/day2/lab1/Greeting.java
+++ src/main/java/io/zipcoder/fundamentals/day2/lab1/Greeting.java
@@ -1,9 +1,17 @@
 package io.zipcoder.fundamentals.day2.lab1;
-
+import java.util.Scanner;
 
 public class Greeting {
 
     public static void main(String[] args){
-
+    	Scanner stdin = new Scanner(System.in);
+    	String userName;
+    	System.out.print("Please enter your name: ");
+    	userName = stdin.nextLine();
+    		if (userName.equals("Alice")) 
+    			System.out.print("Hello, " + userName);
+    		if (userName.equals("Bob"))
+    			System.out.print("Hello, " + userName);
+    	stdin.close();
     }
 }
